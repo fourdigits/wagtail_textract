bf3db16431d77aaf32656bd3794b35f31972d8e5
Kees Hink <keeshink@gmail.com>
2018-05-02 11:13:16 +0200

    Make transcribe() use textract to fill transcription.

--- README.rst
+++ README.rst
@@ -6,6 +6,7 @@ https://github.com/wagtail/wagtail/issues/542
 Installation
 ------------
 
+- Install the Texttract dependencies: http://textract.readthedocs.io/en/latest/installation.html
 - Add `wagtail_textract` to your requirements and/or `pip install wagtail_textract`
 - Add to your Django `INSTALLED_APPS`, after `wagtail.documents`.
 - Put `WAGTAILDOCS_DOCUMENT_MODEL = "wagtail_textract.document"` in your Django settings.
--- src/wagtail_textract/models.py
+++ src/wagtail_textract/models.py
@@ -1,5 +1,10 @@
+import logging
+import textract
+
 from django.db import models
 from wagtail.documents.models import Document
+
+logger = logging.getLogger(__name__)
 
 
 class Document(Document):
@@ -8,4 +13,18 @@ class Document(Document):
 
     def transcribe(self):
         """Extract text from file."""
-        pass
+        try:
+            text = textract.process(self.file.path)
+        except Exception as err:
+            text = None
+            logger.error(
+                'Text extraction error with file {file}: {message}'.format(
+                    file=self.filename,
+                    message=str(err),
+                )
+            )
+
+        if text:
+            self.transcription = text.decode()
+            self.save()
+            logger.info("Saved transcription: %s" % text)
