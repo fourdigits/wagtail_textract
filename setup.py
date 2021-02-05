ff6f4a7d7e629545a41a7a1435b311413eccf1d3
Thijs Kramer <thijs@fourdigits.nl>
2021-02-05 20:13:43 +0100

    update supported Wagtail, Django and Python versions to the current standards

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 
 install_requires = [
-    "wagtail>=2,<2.6",
+    "wagtail>=2.7,<2.13",
     "textract",
 ]
 
@@ -51,10 +51,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Framework :: Django',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.1',
         'Framework :: Wagtail',
         'Framework :: Wagtail :: 2',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
