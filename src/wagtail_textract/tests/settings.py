5fc99ac6eed9c9c32a39564e588ba2a7bbb2b764
Thijs Kramer <thijs@fourdigits.nl>
2021-02-05 20:29:30 +0100

    use a proper list to concat with installed apps

--- src/wagtail_textract/tests/settings.py
+++ src/wagtail_textract/tests/settings.py
@@ -2,4 +2,4 @@ from wagtail.tests.settings import *
 
 MEDIA_ROOT = '.'
 WAGTAILDOCS_DOCUMENT_MODEL = 'wagtail_textract.document'
-INSTALLED_APPS = INSTALLED_APPS + ('wagtail_textract',)
+INSTALLED_APPS = INSTALLED_APPS + ['wagtail_textract']
